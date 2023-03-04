922bfe868cb4000e0368b562967a0346b034a842
Maksim Ugay <maxim9733012m@gmail.com>
2023-03-04 23:12:04 +0500

    lesson-1

---
+++ lesson-1.js
@@ -0,0 +1,26 @@
+var userName = prompt('Имя?');
+var userAge = +prompt('Возраст?');
+var example1 = prompt('6 + 3 = ?');
+alert('Правильный ответ: 9 , Ваш ответ: ' + example1);
+var example2 = prompt('22 - 10 = ?');
+alert('Правильный ответ: 12 , Ваш ответ: ' + example2);
+var example3 = prompt('5 * 5 = ?');
+alert('Правильный ответ: 25 , Ваш ответ: ' + example3);
+var example4 = prompt('12 % 5 = ?');
+alert('Правильный ответ: 2 , Ваш ответ: ' + example4);
+
+
+console.log('Вас зовут: ' + userName);
+console.log('Ваш возраст: ' + userAge);
+console.log('Правильный ответ: 9 , Ваш ответ:' + example1);
+console.log('Правильный ответ: 12 , Ваш ответ:' + example2);
+console.log('Правильный ответ: 25 , Ваш ответ:' + example3);
+console.log('Правильный ответ: 2 , Ваш ответ:' + example4);
+
+var x = +prompt('Введите число x');
+var y = +prompt('Введите число y');
+var z = +prompt('Введите число z');
+
+var res = (x+y+z) / 3;
+
+alert('Среднее арифметическое: ' + res);
